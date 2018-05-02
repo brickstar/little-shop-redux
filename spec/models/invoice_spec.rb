8fd85f1a7686d4f9015b47ef60660280338bbfff
brickstar <brickstar@gmail.com>
2018-05-02 17:43:30 -0600

    fixes status

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe Invoice do
   describe "Validations" do
     it "is invalid without a merchant_id" do
-      invoice = Invoice.create(merchant_id: nil)
+      invoice = Invoice.create(merchant_id: nil, status: 'pending')
 
       expect(invoice).to_not be_valid
     end
     it "is valid with a merchant_id" do
-      invoice = Invoice.create(merchant_id: 1)
+      invoice = Invoice.create(merchant_id: 1, status: 'pending')
 
       expect(invoice).to be_valid
     end
