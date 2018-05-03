6b2f63336d4b53330f32e8fa8370f80d9b876caa
brickstar <brickstar@gmail.com>
2018-05-03 13:02:59 -0600

    hey!!!

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_03_145252) do
+ActiveRecord::Schema.define(version: 2018_05_03_150130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2018_05_03_145252) do
     t.text "description"
     t.integer "price"
     t.text "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "merchants", force: :cascade do |t|
