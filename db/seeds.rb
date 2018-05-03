20a0c92e423597d64da510a41563009b2067e741
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-02 20:30:32 -0600

    Adding merchant parser for merchant csv.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 require 'CSV'
 
 merchants = CSV.open('./data/merchants.csv', headers: true, header_converters: :symbol)
-invoices = CSV.open('./data/invoices.csv', headers: true, header_converters: :symbol)
-items = CSV.open('./data/items.csv', headers: true, header_converters: :symbol)
+# invoices = CSV.open('./data/invoices.csv', headers: true, header_converters: :symbol)
+# items = CSV.open('./data/items.csv', headers: true, header_converters: :symbol)
 
 
 merchants.each do |merchant|
@@ -12,20 +12,20 @@ merchants.each do |merchant|
     updated_at: merchant[:updated_at])
   end
 
-  invoices.each do |invoice|
-    Invoice.create(id: invoice[:id],
-      customer_id: invoice[:customer_id],
-      merchant_id: invoice[:merchant_id],
-      status: invoice[:status],
-      created_at: invoice[:created_at],
-      updated_at: invoice[:updated_at])
-    end
-
-    items.each do |item|
-      Item.create(id: item[:id],
-        customer_id: item[:customer_id],
-        merchant_id: item[:merchant_id],
-        status: item[:status],
-        created_at: item[:created_at],
-        updated_at: item[:updated_at])
-      end
+  # invoices.each do |invoice|
+  #   Invoice.create(id: invoice[:id],
+  #     customer_id: invoice[:customer_id],
+  #     merchant_id: invoice[:merchant_id],
+  #     status: invoice[:status],
+  #     created_at: invoice[:created_at],
+  #     updated_at: invoice[:updated_at])
+  #   end
+  #
+  #   items.each do |item|
+  #     Item.create(id: item[:id],
+  #       customer_id: item[:customer_id],
+  #       merchant_id: item[:merchant_id],
+  #       status: item[:status],
+  #       created_at: item[:created_at],
+  #       updated_at: item[:updated_at])
+  #     end
