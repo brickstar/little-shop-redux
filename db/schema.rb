5a48869dab8a286addf13675e74f0d6663865a49
brickstar <brickstar@gmail.com>
2018-05-03 08:59:19 -0600

    adds timestamps to Merchant and Invoice

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_02_232405) do
+ActiveRecord::Schema.define(version: 2018_05_03_145252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2018_05_02_232405) do
   create_table "invoices", force: :cascade do |t|
     t.integer "merchant_id"
     t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|
@@ -29,6 +31,8 @@ ActiveRecord::Schema.define(version: 2018_05_02_232405) do
 
   create_table "merchants", force: :cascade do |t|
     t.text "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
