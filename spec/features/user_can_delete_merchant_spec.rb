34666fb114005a7d830718c82b4001314924cd29
brickstar <brickstar@gmail.com>
2018-05-05 14:22:23 -0600

    adds shitty test for delete merchant

---
+++ spec/features/user_can_delete_merchant_spec.rb
@@ -0,0 +1,19 @@
+require 'pry'
+
+RSpec.describe 'Visitors' do
+  context 'who click delete button' do
+    it 'should delete specified merchant and redirected to merchant index' do
+
+      merchant_1 = Merchant.create(name: 'Subreenuh')
+      merchant_2 = Merchant.create(name: 'ROBBB')
+      merchant_3 = Merchant.create(name: 'Mike')
+
+      expect(Merchant.count).to eq(3)
+
+      click_button('delete')
+
+      expect(current_path).to eq('/merchants')
+      expect(Merchant.count).to eq(2)
+    end
+  end
+end
