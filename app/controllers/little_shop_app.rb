b8f31217f87dcde8705b349e40d601f4c12ed954
rcm32000 <rcm32000@yahoo.com>
2018-05-06 14:51:20 -0600

    Mechant Delete button function working

--- app/controllers/little_shop_app.rb
+++ app/controllers/little_shop_app.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class LittleShopApp < Sinatra::Base
   get '/merchants' do
     @merchants = Merchant.all
@@ -9,10 +11,11 @@ class LittleShopApp < Sinatra::Base
   #   erb :"merchants/"
   # end
   #
-  # delete '/merchants/:id' do |id|
-  #   Merchant.destroy(id.to_i)
-  # end
-  #
+  delete '/merchants/:id' do |id|
+    Merchant.destroy(id.to_i)
+    redirect '/merchants'
+  end
+
   # get '/merchants/single_merchant/:id' do
   #   @merchant = Merchant.find(params[:id])
   #   erb :"merchants/single_merchant"
--- spec/features/user_can_delete_merchant_spec.rb
+++ spec/features/user_can_delete_merchant_spec.rb
@@ -5,15 +5,21 @@ RSpec.describe 'Visitors' do
     it 'should delete specified merchant and redirected to merchant index' do
 
       merchant_1 = Merchant.create(name: 'Subreenuh')
-      # merchant_2 = Merchant.create(name: 'ROBBB')
-      # merchant_3 = Merchant.create(name: 'Mike')
-      expect(Merchant.count).to eq(1)
+      merchant_2 = Merchant.create(name: 'ROBBB')
+      merchant_3 = Merchant.create(name: 'MAHTTTT')
 
-      # save_and_open_page
-      click_button('delete-button')
+      expect(Merchant.count).to eq(3)
+
+      visit('/merchants')
+
+      within ".merchant-#{merchant_2.id}" do
+        click_button('Delete')
+      end
 
       expect(current_path).to eq('/merchants')
-      expect(Merchant.count).to eq(0)
+      expect(Merchant.count).to eq(2)
+      expect(page).to_not have_content('ROBBB')
+      expect(page).to have_content('MAHTTTT')
     end
   end
 end
