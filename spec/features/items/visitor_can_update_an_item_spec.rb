85fa9d71b5309fa3b6ca7aad87692327900f3c28
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-08 18:43:22 -0600

    Worked on tests with my awesome mentor

--- spec/features/items/visitor_can_update_an_item_spec.rb
+++ spec/features/items/visitor_can_update_an_item_spec.rb
@@ -3,14 +3,17 @@ RSpec.describe 'Visitors' do
     it 'should update an existing item and should be redirected to said item page' do
       merchant = Merchant.create(name: 'Dingle')
 
-      item = Item.create(title: 'Sabrina', description: 'Awesome', price: '100', merchant_id: "#{merchant.id}")
+      item = Item.create(title: 'Sabrina',
+                          description: 'Awesome',
+                          price: '100',
+                          merchant_id: "#{merchant.id}")
 
       visit("/items/#{item.id}")
 
       click_link('Edit')
       expect(current_path).to eq("/items/#{item.id}/edit")
 
-      fill_in "title", with: 'Pearly'
+      fill_in "title", with: 'Pearly', visible: false
       fill_in "description", with: 'Adorable'
       fill_in "price", with: '1000000'
 
