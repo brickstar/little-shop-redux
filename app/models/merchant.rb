bc919bbf2004ab98b36f3ef8c6ca21c243e49916
Sabrina Robinson <sabrinarob@gmail.com>
2018-05-09 09:58:21 -0600

    adds joins for merchants and items and tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,4 +3,10 @@ class Merchant < ActiveRecord::Base
 
   has_many :items
   has_many :invoices
+
+  def self.merchant_item_info
+    Merchant.joins(:items).select("merchants.*, sum(items.price)
+    AS total_cost, count(items.id) AS items_count, avg(items.price)
+    AS average_price").group("merchants.id")
+  end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,3 +12,36 @@ RSpec.describe Merchant do
     end
   end
 end
+
+  describe 'Class Methods' do
+    context '.merchant_item_info' do
+      it "should return total cost, items count and average price" do
+
+      merchant = Merchant.create!(name: 'Subreenah')
+
+      item_1 = Item.create!(title: 'Polyphonic Spree',
+                           description: 'Symphonic Psych Rock',
+                           price: 10,
+                           image: 'https://www.polyphonicspree.com',
+                           merchant_id: 1)
+      item_2 = Item.create!(title: 'Heartless Bastards',
+                           description: 'Rock',
+                           price: 20,
+                           image: 'https://www.heartlessbastards.com',
+                           merchant_id: 1)
+      item_3 = Item.create!(title: 'The Black Angels',
+                           description: 'Psych Rock',
+                           price: 30,
+                           image: 'https://www.blackangels.com',
+                           merchant_id: 1)
+
+      total_cost = item_1.price + item_2.price + item_3.price
+      item_count = 3
+      average_price = total_cost / item_count
+
+      expect(Merchant.merchant_item_info[0].total_cost).to eq(total_cost)
+      expect(Merchant.merchant_item_info[0].items_count).to eq(item_count)
+      expect(Merchant.merchant_item_info[0].average_price).to eq(average_price)
+    end
+  end
+end
