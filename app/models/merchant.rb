5bbcd0da9c52e1ce1bbc5fa33fb995eedf7b8ae4
brickstar <brickstar@gmail.com>
2018-05-08 23:45:00 -0600

    adds first two class methods tests

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,9 +1,4 @@
 class Merchant < ActiveRecord::Base
   validates :name, presence: true
   has_many :items
-
-  def self.delete
-    merchant = Merchant.find(params[:id])
-    merchant.delete
-  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 require './app/models/merchant.rb'
 require './app/models/invoice.rb'
 require './app/models/item.rb'
+require './app/models/invoice_item.rb'
 require 'csv'
 require 'pry'
 
@@ -28,4 +29,14 @@ CSV.foreach('./data/items.csv', headers: true, header_converters: :symbol) do |i
     created_at: item[:created_at],
     updated_at: item[:updated_at],
     merchant_id: item[:merchant_id])
+end
+
+CSV.foreach('./data/invoice_items.csv', headers: true, header_converters: :symbol) do |invoice_item|
+  InvoiceItem.create(id: invoice_item[:id],
+    item_id: invoice_item[:item_id],
+    invoice_id: invoice_item[:invoice_id],
+    unit_price: invoice_item[:unit_price],
+    quantity: invoice_item[:quantity],
+    created_at: invoice_item[:created_at],
+    updated_at: invoice_item[:updated_at])
 end
