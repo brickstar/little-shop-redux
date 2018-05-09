68a2f4095e6aeb8596e7b131eeaaf4e20cdb9ff0
brickstar <brickstar@gmail.com>
2018-05-08 23:59:51 -0600

    adds total item count

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,12 @@
+require 'pry'
 class Item < ActiveRecord::Base
   validates :title, presence: true
   validates :description, presence: true
   validates :price, presence: true
   validates :image, presence: false
   belongs_to :merchant
+
+  def self.total_item_count
+    all.count
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -5,47 +5,55 @@ RSpec.describe Item do
 
       expect(item).to_not be_valid
     end
+
+
+
     it 'is valid with a title' do
       item = Item.create(title: 'Bob', description: 'yo yo', price: 47, image: 'valid')
 
       expect(item).to be_valid
     end
+  end
+end
 
-    descirbe 'Class Methods' do
-      context '.total_item_count' do
-        it 'should return a total count of items'
-        item_1 = Item.create(title: 'Polyphonic Spree',
-                             description: 'Symphonic Psych Rock',
-                             price: 47,
-                             image: 'https://www.polyphonicspree.com')
-        item_2 = Item.create(title: 'Heartless Bastards',
-                             description: 'Rock',
-                             price: 13,
-                             image: 'https://www.heartlessbastards.com')
-        item_3 = Item.create(title: 'The Black Angels',
-                             description: 'Psych Rock',
-                             price: 77,
-                             image: 'https://www.blackangels.com')
 
-        expect(Item.total_item_count).to eq(3)
-      end
+  describe 'Class Methods' do
+    context '.total_item_count' do
+      it 'should return a total count of items' do
+      item_1 = Item.create(title: 'Polyphonic Spree',
+                           description: 'Symphonic Psych Rock',
+                           price: 47,
+                           image: 'https://www.polyphonicspree.com')
+      item_2 = Item.create(title: 'Heartless Bastards',
+                           description: 'Rock',
+                           price: 13,
+                           image: 'https://www.heartlessbastards.com')
+      item_3 = Item.create(title: 'The Black Angels',
+                           description: 'Psych Rock',
+                           price: 77,
+                           image: 'https://www.blackangels.com')
 
-      context '.average_price_per_item' do
-        it 'returns average price for item'
-        item_1 = Item.create(title: 'Polyphonic Spree',
-                             description: 'Symphonic Psych Rock',
-                             price: 10,
-                             image: 'https://www.polyphonicspree.com')
-        item_2 = Item.create(title: 'Heartless Bastards',
-                             description: 'Rock',
-                             price: 20,
-                             image: 'https://www.heartlessbastards.com')
-        item_3 = Item.create(title: 'The Black Angels',
-                             description: 'Psych Rock',
-                             price: 40,
-                             image: 'https://www.blackangels.com')
+      expect(Item.total_item_count).to eq(3)
+    end
+  end
 
-        expect(Item.average_price_per_item).to eq(20)
+
+    context '.average_price_per_item' do
+      it 'returns average price for item' do
+      item_1 = Item.create(title: 'Polyphonic Spree',
+                           description: 'Symphonic Psych Rock',
+                           price: 10,
+                           image: 'https://www.polyphonicspree.com')
+      item_2 = Item.create(title: 'Heartless Bastards',
+                           description: 'Rock',
+                           price: 20,
+                           image: 'https://www.heartlessbastards.com')
+      item_3 = Item.create(title: 'The Black Angels',
+                           description: 'Psych Rock',
+                           price: 40,
+                           image: 'https://www.blackangels.com')
+
+      expect(Item.average_price_per_item).to eq(20)
     end
   end
 end
